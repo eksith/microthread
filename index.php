4ef0247a4ea68ab3c2c421a3c16deb8d78738a55
Eksith Rodrigo <eksith@live.com>
2014-11-13 17:53:46 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+// Testing page. Thar be dragons!
+
+ini_set( 'display_errors', '1' ); // Remove this in production
+set_time_limit( 3600 );
+
+$initialMemory = memory_get_usage();
+
+/**
+ * Execution time
+ */
+define( 'START',	-microtime( true ) ); // Use with + microtime(true);
+
+/**
+ * Application base path.
+ */
+define( 'PATH',		realpath( dirname( __FILE__ ) ) . '/' );
+
+function formatBytes( $bytes, $precision = 2 ) {
+	$units = array('B', 'KB', 'MB', 'GB', 'TB');
+	
+	$bytes = max( $bytes, 0 );
+	$pow = floor( ( $bytes ? log( $bytes ) : 0 ) / log( 1024 ) );
+	$pow = min( $pow, count( $units ) - 1 );
+	
+	return round( $bytes, $precision ) . ' ' . $units[$pow];
+}
+
+$initialMemory = 'Initial ' . formatBytes( $initialMemory ) . " bytes";
+
+/**
+ * Microthread core configuration
+ */
+require( PATH . 'vendor/Microthread/bootstrap.php' );
