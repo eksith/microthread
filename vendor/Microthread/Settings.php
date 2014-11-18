f9dedbdd09e285cfc2b509dfb52f78640cbcb018
Eksith Rodrigo <eksith@live.com>
2014-11-18 15:39:30 -0500

    Create Settings.php

---
+++ vendor/Microthread/Settings.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Saving, creating, updating application settings
+ *
+ * @author Eksith Rodrigo <reksith at gmail.com>
+ * @license http://opensource.org/licenses/ISC ISC License
+ * @version 0.1
+ */
+namespace Microthread;
+use Microthread\Models;
+
+class Settings {
+	
+	private static $loaded = array();
+	
+	public static function getConfig( $id, $decrypt = false ) {
+		if ( in_array( $id , self::$loaded )  ) {
+			return self::$loaded[$id];
+		}
+		
+		$setting		= Models\Setting::find( $id );
+		self::$loaded[$id]	= $setting->decoded;
+		
+		return self::$loaded[$id];
+	}
+	
+	public static function setConfig( $id, $data, $encrypt = false ) {
+		$setting		= new Models\Setting();
+		$setting->id		= $id;
+		
+		if ( in_array( $id , self::$loaded )  ) {
+			self::$loaded[$id]	= $data;
+			$setting->data		= $data;
+		}
+		
+		return $setting->save();
+	}
+	
+	
+	public static function updateFromFile( $id ) {
+		$setting = Models\Setting::find( 
+				array( 'id' => $id, 'data'=> true )
+			);
+		
+		if ( !isset( $setting->id ) ) {
+			$setting	= new Models\Setting();
+			$setting->id	= $id;
+		}
+		
+		$setting->data		= Models\Setting::getFileConfig( $id );
+		$setting->save();
+	}
+}
